89575a6c829740f81b431eef4ffe53af11d78405
John Guckian <johnguckian@Johns-MacBook-Air.local>
2024-11-20 19:54:41 -0700

    added javascript and css

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 document.getElementById('addRecipe').addEventListener('click', addRecipe);
 
-function addRecipe(){
-    
+function addRecipe() {
+    const newRecipe = document.createElement("div");
+    newRecipe.className = "recipe";
+
+    const remove = document.createElement("button");
+    remove.id = "remove";
+    remove.textContent = "-";
+    remove.addEventListener('click', function () {
+        newRecipe.remove();
+    });
+    newRecipe.appendChild(remove);
+
+    const imgInput = document.createElement("input");
+    imgInput.type = "file"
+    imgInput.className = "recipe-img";
+    newRecipe.appendChild(imgInput);
+
+    const description = document.createElement("textarea");
+    description.placeholder = "Write Recipe...";
+    description.className = "description";
+    newRecipe.appendChild(description);
+
+    document.body.appendChild(newRecipe);
 }
